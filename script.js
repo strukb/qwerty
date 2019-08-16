38b15f374eae85499fdac083a4f7745d2aafbce9
strukb <struk.bohdan@comp-sc.if.ua>
2019-08-16 17:47:20 +0300

    dsf

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var active = false;
+$(".show-more a").on("click", function() {
+  $('li.hidden').slideToggle();
+  $(this).text(active ? 'Show more' : 'Show less')
+  active = !active;
+});
+})
