cdee8d1a3ba3947e208c2634fc08709763948875
vkhomyn95 <vkhomyn95@gmail.com>
2019-08-21 22:08:54 +0300

    last changes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     var active = false;
-$(".show-more a").on("click", function() {
-  $('li.hidden').slideToggle();
-  $(this).text(active ? 'Show more' : 'Show less')
-  active = !active;
-});
+	$(".show-more a").on("click", function() {
+	  $('li.hidden').slideToggle();
+	  $(this).text(active ? 'Show more' : 'Show less')
+	  active = !active;
+	});
+	
 })
