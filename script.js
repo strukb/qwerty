4ff4d8b1fa09479332d48b7a73a20e04f83d5985
vkhomyn95 <vkhomyn95@gmail.com>
2019-12-17 02:51:24 +0200

    efef

--- script.js
+++ script.js
@@ -2,8 +2,32 @@ $(document).ready(function(){
     var active = false;
 	$(".show-more a").on("click", function() {
 	  $('li.hidden').slideToggle();
-	  $(this).text(active ? 'Show more' : 'Show less')
+	  $(this).text(active ? 'Показати більше' : 'Показати менше')
 	  active = !active;
 	});
-	
+	// Popup Al
+var modal = document.getElementById('myModal');
+
+// Kipi açan düğmeyi al
+var btn = document.getElementById("myBtn");
+
+// Kipi kapatan <span> öğesini edinin
+var span = document.getElementsByClassName("close")[0];
+
+// Kullanıcı düğmeyi tıklattığında
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Kullanıcı <span> (x) düğmesini tıkladığında, popup
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Kullanıcı modelden başka herhangi bir yeri tıklattıysa, onu kapatın.
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 })
